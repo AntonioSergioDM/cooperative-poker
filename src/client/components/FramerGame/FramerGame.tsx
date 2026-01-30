d40adc5b0e1724d6c073cf8d6433ef536f2b92cf
Serginho <antoniosergio0606@gmail.com>
2026-01-30 21:16:35 +0000

    now i can see the names

--- src/client/components/FramerGame/FramerGame.tsx
+++ src/client/components/FramerGame/FramerGame.tsx
@@ -20,8 +20,8 @@ import { useSocket } from '@/client/tools/useSocket';
 import PlayerHand from '@/client/components/Players/PlayerHand';
 import { BIG_CARD, SMALL_CARD } from '@/client/components/AnimatedCard';
 
+import { Typography } from '@mui/material';
 import Table from './Table';
-import {Typography} from "@mui/material";
 
 type FramerGameProps = {
   gameState: GameState;
@@ -86,28 +86,32 @@ const FramerGame = (props: FramerGameProps) => {
       {players.map((player, idx) => {
         const angle = (bottomIdx - idx) * ((2 * Math.PI) / players.length) - (3 * (Math.PI / 6));
 
-        const bottom = ((Math.sin(angle) * 100) / 2.3) + 45;
+        const bottom = ((Math.sin(angle) * 100) / 2.3) + 40;
         const left = ((Math.cos(angle) * 100) / 2.3) + 45;
 
-        const style = {
+        const position = {
           bottom: `${bottom.toFixed(2)}%`,
           left: `${left.toFixed(2)}%`,
+        };
+
+        const rotate = {
           '--tw-rotate': `${angle * (180 / Math.PI) + 90}deg`,
           transform: 'translate(var(--tw-translate-x), var(--tw-translate-y)) rotate(var(--tw-rotate)) '
             + 'skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y))',
+          width: '120px',
+          height: '120px',
         };
 
         const cards = bottomIdx === idx ? playerState.hand : Array(gameState.hands[idx]).fill(0);
-        // TODO make the name appear
         return (
-          // TODO check what this is
-          // eslint-disable-next-line react/jsx-key
-          <div className="fixed flex row justify-center gap-20" style={style}>
-            <PlayerHand
-              cardWidth={bottomIdx === idx ? BIG_CARD : SMALL_CARD}
-              cards={cards}
-              name={player.name}
-            />
+          <div className="fixed flex flex-col justify-center gap-4" style={position} key={player.name}>
+            <div style={rotate}>
+              <PlayerHand
+                cardWidth={bottomIdx === idx ? BIG_CARD : SMALL_CARD}
+                cards={cards}
+                name={player.name}
+              />
+            </div>
 
             <Typography>{player.name}</Typography>
           </div>
