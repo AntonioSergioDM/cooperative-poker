8ce2c85202e542323ec88b10862548219369e3b3
Serginho <antoniosergio0606@gmail.com>
2026-02-16 19:54:18 +0000

    Revert "Merge pull request #23 from AntonioSergioDM/20-winninglose-sound-effect"
    This reverts commit bf74580b9d706e064b541ec722f0a83d6d21fee9, reversing
    changes made to 82aa8606633181296c2ad210d5e81fccebe2810c.

--- src/client/tools/sounds.ts
+++ src/client/tools/sounds.ts
@@ -1,13 +1,9 @@
 const sounds = {
   beep: '/sounds/beep.mp3',
   // beep: '/sounds/beep1.mp3',
-  win: '/sounds/win.mp3',
-  lose: '/sounds/lose.mp3',
 };
 
-export type SoundEffect = keyof typeof sounds;
-
-export const sound = (path: SoundEffect, volume: number = 0.2) => {
+export const sound = (path: 'beep', volume: number = 0.2) => {
   const audio = new Audio(sounds[path]);
   audio.volume = volume || 0.2;
   audio.play().catch((e) => {
