fbff0a15ff08b5205e59737ec8dcea35aa3ec1fa
Serginho <antoniosergio0606@gmail.com>
2026-03-14 11:17:26 +0000

    adjustments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,7 +21,7 @@ const MyApp = (props: AppProps) => {
     <AppCacheProvider {...props}>
       <Head>
         <title>Cooperative Poker</title>
-        <meta name="viewport" content="width=1200" />
+        <meta name="viewport" content="width=980" />
       </Head>
 
       <ThemeProvider theme={theme}>
